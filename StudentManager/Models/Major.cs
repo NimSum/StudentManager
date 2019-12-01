655b356c8aca216e29644f327e61a5674848ec00
Nimrod Garcia <nimm.garcia@gmail.com>
2019-12-01 00:28:57 -0600

    Create student major model class

---
+++ StudentManager/Models/Major.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace StudentManager.Models
+{
+    public class Major
+    {
+        [Key]
+        public int MajorId { get; set; }
+        [MaxLength(100)]
+        [Required]
+        public string Name { get; set; }
+        [MaxLength(300)]
+        [Required]
+        public string Details { get; set; }
+
+        public Major()
+        {
+        }
+    }
+}
