22d60c16960aa58da70d726c6c94c254f9c49ffb
Nimrod Garcia <nimm.garcia@gmail.com>
2019-12-01 00:29:25 -0600

    Create student model class

---
+++ StudentManager/Models/Student.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace StudentManager.Models
+{
+    public class Student
+    {
+        [Key]
+        public int StudentId { get; set; }
+        [MaxLength(150)]
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string SchoolYear { get; set; }
+        [Required]
+        [ForeignKey("Major")]
+        public int MajorRefId { get; set; }
+        public Major Major { get; set; }
+    }
+}
