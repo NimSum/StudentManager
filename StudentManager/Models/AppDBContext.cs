563a3aab976cef87fd1703b17fe73114e29d3206
Nimrod Garcia <nimm.garcia@gmail.com>
2019-12-01 00:27:59 -0600

    Create app database context class

---
+++ StudentManager/Models/AppDBContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+
+namespace StudentManager.Models
+{
+    public class AppDBContext : DbContext
+    {
+        public AppDBContext(DbContextOptions<AppDBContext> options): base(options)
+        {
+        }
+
+        public DbSet<Major> Majors { get; set; }
+        public DbSet<Student> Students { get; set; }
+    }
+}
