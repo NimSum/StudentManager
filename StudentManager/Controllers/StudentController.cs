5482d664ceaf03422f5d79961ee0701a0c563d5c
Nimrod Garcia <nimm.garcia@gmail.com>
2019-12-01 23:37:16 -0600

    Create student controller, index list view

---
+++ StudentManager/Controllers/StudentController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using StudentManager.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace StudentManager.Controllers
+{
+    public class StudentController : Controller
+    {
+        private readonly AppDBContext _context;
+
+        public StudentController(AppDBContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> IndexAsync()
+        {
+            return View(await _context.Students.ToListAsync());
+        }
+
+    }
+}
