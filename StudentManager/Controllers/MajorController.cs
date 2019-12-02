e5da83ff0366a73741e410ce2a8d6b440bfe1ff7
Nimrod Garcia <nimm.garcia@gmail.com>
2019-12-01 22:11:01 -0600

    Provide database context to controller

--- StudentManager/Controllers/MajorController.cs
+++ StudentManager/Controllers/MajorController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using StudentManager.Models;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -10,6 +12,13 @@ namespace StudentManager.Controllers
 {
     public class MajorController : Controller
     {
+        private readonly AppDBContext _context;
+
+        public MajorController(AppDBContext context)
+        {
+            _context = context;
+        }
+
         // GET: /<controller>/
         public IActionResult Index()
         {
